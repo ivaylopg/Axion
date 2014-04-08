586f6ab3dbf2f93b61968914560719c26176baa8
Ivaylo Getov <contact@ivaylogetov.com>
2014-04-08 13:43:10 -0400

    Implemented fades between states
    Former-commit-id: 397d821367958a2e8dac91262e95090723683ec8

--- src/fader.cpp
+++ src/fader.cpp
@@ -12,18 +12,42 @@ void Fader::setup(){
 	}
 	initialized = true;
     
-    fadFac = 0.77;
+    fadFac = 0.775;
     fadAlpha = 0.0;
     //fadAlpha = 100.0;
     
     fadz = ofGetHeight() * fadFac;
+    fullyCovered = false;
+    movingOn = false;
+    revealing = false;
+    speed = 3;
+    holdTime = 1.0;
     
+    isFading = false;
 
 }
 
 //--------------------------------------------------------------
 void Fader::update(){
     fadz = ofGetHeight() * fadFac;
+    
+    if (movingOn && !revealing) {
+        if (fadAlpha >= 255) {
+            fullyCovered = true;
+        }
+        if (fadAlpha < (255 + (holdTime * ofGetFrameRate() * speed)) ) {
+            
+            fadAlpha+=speed;
+        } else {
+            revealing = true;
+            fadAlpha = 255;
+        }
+    }
+    if (movingOn && revealing) {
+        if (fadAlpha > 0) {
+            fadAlpha-=speed;
+        }
+    }
 }
 
 //--------------------------------------------------------------
@@ -31,14 +55,57 @@ void Fader::draw(){
     
     ofPushMatrix();
     ofTranslate(ofGetWidth()/2, ofGetHeight()/2, fadz);
-    ofSetColor(255,0,0, fadAlpha);
+    ofSetColor(0, ofClamp(fadAlpha,0,255));
     ofRect(-ofGetWidth()/2, -ofGetHeight()/2, ofGetWidth(), ofGetHeight());
-    
-    ofSetColor(255, 255);
-    ofDrawBitmapString(ofToString(fadz) + " : " + ofToString(ofGetWidth()) + " : " + ofToString(ofGetHeight()), 20, 20);
-    
-    
     ofPopMatrix();
     
+    if (movingOn && fullyCovered && revealing && fadAlpha <= 0) {
+        fadAlpha = 0;
+        movingOn = false;
+        revealing = false;
+        fullyCovered = false;
+        isFading = false;
+    }
+}
+
+//--------------------------------------------------------------
+void Fader::drawTest(){
     
+    ofPushMatrix();
+    ofTranslate(ofGetWidth()/2, ofGetHeight()/2, fadz);
+    ofSetColor(0, 255);
+    ofRect(-ofGetWidth()/2, -ofGetHeight()/2, ofGetWidth(), ofGetHeight());
+    ofPopMatrix();
+    
+    ofSetColor(255, 255);
+    ofDrawBitmapString(ofToString(fadz+1) + " : " + ofToString(ofGetWidth()) + " : " + ofToString(ofGetHeight()), 20, 20);
+}
+
+//--------------------------------------------------------------
+void Fader::moveOn(){
+    if (!isFading) {
+        isFading = true;
+        movingOn = true;
+    }
+}
+
+//--------------------------------------------------------------
+float Fader::getAlpha(){
+    return ofClamp(fadAlpha, 0, 255);
+}
+
+//--------------------------------------------------------------
+void Fader::fadeUp(){
+    if (!isFading) {
+        isFading = true;
+        movingOn = true;
+        fadAlpha = 255;
+        revealing = true;
+        fullyCovered = true;
+    }
+}
+
+//--------------------------------------------------------------
+bool Fader::fullCover(){
+    return fullyCovered;
 }
--- src/fader.h
+++ src/fader.h
@@ -8,12 +8,24 @@ public:
     void setup();
     void update();
     void draw();
+    void moveOn();
+    void fadeUp();
+    bool fullCover();
     
     bool initialized = false;
+    float speed;
+    float holdTime;         // in seconds
+    
+    void drawTest();
+    float getAlpha();
     
 protected:
     float fadFac;
     float fadz;
     float fadAlpha;
+    bool movingOn;
+    bool revealing;
+    bool fullyCovered;
+    bool isFading;
     
 };
