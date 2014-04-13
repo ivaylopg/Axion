72c051eb951daf99587fc470c9159c420e3e3eb6
Ivaylo Getov <contact@ivaylogetov.com>
2014-04-12 21:15:12 -0400

    Added a comment to main.cpp
    Former-commit-id: 87d7fce6cc55e04014fadfd1be02fe594564480b

--- src/main.cpp
+++ src/main.cpp
@@ -7,7 +7,8 @@ int main( ){
     //ofSetupOpenGL(1024,576,OF_FULLSCREEN);        // Different size of 16:9
     
     ofSetDataPathRoot("../Resources/data/");        // If not building via the XCode Project on a Mac, you WILL need to comment this out!
-                                                    // This is to make self-contained app (via xcode) on Mac.
+                                                    // This is to make self-contained app (via xcode) on Mac, along with this run script:
+                                                    // cp -r bin/data "$TARGET_BUILD_DIR/$PRODUCT_NAME.app/Contents/Resources";
     
 	ofRunApp(new controller());
 }
