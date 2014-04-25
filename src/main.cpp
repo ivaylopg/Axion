6e55ce5293a14bc59dc51ee44fc6330060711e00
Ivaylo Getov <contact@ivaylogetov.com>
2014-04-25 12:26:17 -0400

    Added EEG and reconfigured video file loading
    Former-commit-id: 2b91e1ddd0969e3d9eced26f31616c7ce6781663

--- src/main.cpp
+++ src/main.cpp
@@ -6,7 +6,7 @@ int main( ){
 	ofSetupOpenGL(1366,768,OF_FULLSCREEN);			// <-------- setup the GL context
     //ofSetupOpenGL(1024,576,OF_FULLSCREEN);        // Different size of 16:9
     
-    ofSetDataPathRoot("../Resources/data/");        // If not building via the XCode Project on a Mac, you WILL need to comment this out!
+    //ofSetDataPathRoot("../Resources/data/");        // If not building via the XCode Project on a Mac, you WILL need to comment this out!
                                                     // This is to make self-contained app (via xcode) on Mac, along with this run script:
                                                     // cp -r bin/data "$TARGET_BUILD_DIR/$PRODUCT_NAME.app/Contents/Resources";
     
