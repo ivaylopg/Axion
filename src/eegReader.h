e506545417418d90bfca8ab4db1a265e12d36025
Ivaylo Getov <contact@ivaylogetov.com>
2014-04-25 12:26:17 -0400

    Added EEG and reconfigured video file loading
    Former-commit-id: b294aab709721cd8846bf8754aed638e537bb643

---
+++ src/eegReader.h
@@ -0,0 +1,45 @@
+#pragma once
+#include "ofMain.h"
+#include "ofxThinkGear.h"
+#include "MSAInterpolator.h"
+
+class EEGreader {
+public:
+    EEGreader();
+    
+    void setup();
+    void update();
+    void draw();
+    void free();
+    void reset();
+    
+    bool initialized = false;
+    
+    vector<ofVec2f> meds;
+    vector<ofVec2f> atts;
+    vector<float> attMedRatio;
+    float ave3s;
+    float ave10s;
+    float ave20s;
+    float diff10s;
+    float diff20s;
+    
+    float result10s;
+    float result20s;
+    
+    float values[10];
+    float mapped[10];
+    float averages[10];
+    float loHi [10][2];
+    int lastBlinkVal;
+    int numBlinks;
+    
+    msa::Interpolator2D spline2D;
+    msa::InterpolationType interpolationType;
+    bool useLength;
+    int numSteps;
+    
+private:
+    ofxThinkGear thinkGear;
+    void blinkListener(float &param);
+};
