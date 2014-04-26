0740386621585bed59a01aac9f9acf24765421e2
Ivaylo Getov <contact@ivaylogetov.com>
2014-04-26 06:02:35 -0400

    Testing with placeholder videos. Logging is on.
    Former-commit-id: 49c8be086bdd83e0df695460e83e15739d120f4e

--- src/main.cpp
+++ src/main.cpp
@@ -9,7 +9,7 @@ int main( ){
     //ofSetDataPathRoot("../Resources/data/");        // If not building via the XCode Project on a Mac, you WILL need to comment this out!
                                                     // This is to make self-contained app (via xcode) on Mac, along with this run script:
                                                     // cp -r bin/data "$TARGET_BUILD_DIR/$PRODUCT_NAME.app/Contents/Resources";
-    ofSetLogLevel(OF_LOG_VERBOSE);
+    ofSetLogLevel(OF_LOG_NOTICE);
     ofLogToFile("TFI_Log.txt", true);
 	ofRunApp(new controller());
 }
