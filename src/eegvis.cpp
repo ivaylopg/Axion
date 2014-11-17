9faf728fedf48de23912fe1a0d059cfc2e65d916
Ivaylo Getov <ivaylo.getov@gmail.com>
2014-11-16 23:13:16 -0500

    added eegvis
    Former-commit-id: 36b770fe4d529be2f7d3532565e26bc98645b7fc

---
+++ src/eegvis.cpp
@@ -0,0 +1,9 @@
+//
+//  eegvis.cpp
+//  Axion
+//
+//  Created by Ivaylo Getov on 11/16/14.
+//
+//
+
+#include "eegvis.h"
---
+++ src/eegvis.h
@@ -0,0 +1,14 @@
+//
+//  eegvis.h
+//  Axion
+//
+//  Created by Ivaylo Getov on 11/16/14.
+//
+//
+
+#ifndef Axion_eegvis_h
+#define Axion_eegvis_h
+
+
+
+#endif
