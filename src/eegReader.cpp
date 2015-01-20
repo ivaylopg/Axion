21d9692071a6b3cc79576a069f2047f471602e68
Ivaylo Getov <ivaylo.getov@gmail.com>
2015-01-20 02:24:14 -0800

    added eegreader class

---
+++ src/eegReader.cpp
@@ -0,0 +1,9 @@
+//
+//  eegReader.cpp
+//  Axion
+//
+//  Created by Ivaylo Getov on 1/20/15.
+//
+//
+
+#include "eegReader.h"
---
+++ src/eegReader.h
@@ -0,0 +1,14 @@
+//
+//  eegReader.h
+//  Axion
+//
+//  Created by Ivaylo Getov on 1/20/15.
+//
+//
+
+#ifndef __Axion__eegReader__
+#define __Axion__eegReader__
+
+#include <stdio.h>
+
+#endif /* defined(__Axion__eegReader__) */
