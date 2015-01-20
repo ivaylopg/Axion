c5b2e5ded6c0049be732b18d39c388ba934d8735
Ivaylo Getov <ivaylo.getov@gmail.com>
2015-01-20 03:06:34 -0800

    added visualizer class

---
+++ src/eegVis.cpp
@@ -0,0 +1,9 @@
+//
+//  eegVis.cpp
+//  Axion
+//
+//  Created by Ivaylo Getov on 1/20/15.
+//
+//
+
+#include "eegVis.h"
---
+++ src/eegVis.h
@@ -0,0 +1,14 @@
+//
+//  eegVis.h
+//  Axion
+//
+//  Created by Ivaylo Getov on 1/20/15.
+//
+//
+
+#ifndef __Axion__eegVis__
+#define __Axion__eegVis__
+
+#include <stdio.h>
+
+#endif /* defined(__Axion__eegVis__) */
