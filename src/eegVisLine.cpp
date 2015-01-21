17f008674f3db537e2651c99693ad8a939f6f808
Ivaylo Getov <contact@ivaylogetov.com>
2015-01-21 07:04:57 -0800

    added visualizer line class

---
+++ src/eegVisLine.cpp
@@ -0,0 +1,9 @@
+//
+//  eegVisLine.cpp
+//  Axion
+//
+//  Created by Ivaylo Getov on 1/21/15.
+//
+//
+
+#include "eegVisLine.h"
---
+++ src/eegVisLine.h
@@ -0,0 +1,14 @@
+//
+//  eegVisLine.h
+//  Axion
+//
+//  Created by Ivaylo Getov on 1/21/15.
+//
+//
+
+#ifndef __Axion__eegVisLine__
+#define __Axion__eegVisLine__
+
+#include <stdio.h>
+
+#endif /* defined(__Axion__eegVisLine__) */
